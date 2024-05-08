2a7fc4033d18b953227b70465143d800d11b75c2
limonreza00 <limonreza3.1416@gmail.com>
2024-05-09 00:16:33 +0600

    Fragment Added

--- app/src/main/java/com/coderscastle/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/coderscastle/bottomnavigation/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,39 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        replaceFragment(HomeFragment())
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
 
+                R.id.profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+
+                R.id.notifications -> {
+                    replaceFragment(NotificationFragment())
+                    true
+                }
+
+                R.id.settings -> {
+                    replaceFragment(SettingsFragment())
+                    true
+                }
+
+                else -> false
+
+            }
+        }
+    }
+     private fun replaceFragment(fragment:Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container,fragment)
+            commit()
+        }
     }
 }
